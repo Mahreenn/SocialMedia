142ec03c0e48cf7679fc20fa91f6d3fdca922caa
Mahreenn <mahreenarasin@gmail.com>
2023-08-07 20:55:40 +0600

    toast

--- Frontend/src/components/Toast.vue
+++ Frontend/src/components/Toast.vue
@@ -1,5 +1,22 @@
 <template>
-    <div >
-        
+    <div 
+        v-if="store.isVisible"
+        class="transition ease-in-out delay-500 duration-500 px-6 py-6 fixed top-full right-8 rounded-xl shadow-xl"
+        :class="store.classes"
+        {{ store.message }}
+    >
     </div>
 </template>
+
+<script>
+import {useToastStore} from '@/stores/toast'
+export default {
+    setup(){
+        const store = useToastStore()
+
+        return {
+            store
+        }
+    },
+}
+</script>
